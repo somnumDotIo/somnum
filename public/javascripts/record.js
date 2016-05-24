e09840ba1fca915a287a2ba9445502c352e2c0d2
Nic Higuera <damagecpl@gmail.com>
2016-05-23 17:38:33 -0700

    adjusted index view

---
+++ public/javascripts/record.js
@@ -0,0 +1,15 @@
+// our recording code goes here
+// navigator.getUserMedia();
+
+
+
+// get this functioning
+
+// alarm clock should be seperate js file
+
+
+// MODULE FUCKIN EXPORTS
+
+
+
+module.exports =
