2b0ec7c2490997b2ab00512d6f9d7bdc30d562de
Richard Craven <rcraven85@gmail.com>
2016-05-26 18:03:45 -0700

    it works!!!

--- main.js
+++ main.js
@@ -4,21 +4,29 @@ var app = angular.module('myClock',[]);
 
 app.controller('clockTime', function($scope, $interval){
 	window.scope = $scope
-	$scope.alarm = false;
+	var balls = true
 	$scope.view={}
+	$scope.alarm = false;
+	$scope.alarmTime = 1;
+	var audio = new Audio('airHorn.wav');
 	var tick = function() {
-	  $scope.view.time = Date.now();
-	  if($scope.alarm === true){
+	 $scope.view.time = Date.now();
+	 console.log($scope.view.time)
+	  if($scope.view.time > $scope.alarmTime && balls === false){
+	  	console.log($scope.alarmTime)
 	  	audio.play()
 	  }
 	}
 	tick();
 	$interval(tick, 1000);
-	var audio = new Audio('airHorn.wav');
 	$scope.timeCheck = function(){
 	}
 
-	$scope.sound = function(){
-		audio.play()
+
+	$scope.alarmSet = function(){
+		$scope.alarmTime = (Date.now() + 5000)
+		console.log('click:' + $scope.alarmTime)
+		balls = false
 	}
+	
 })
